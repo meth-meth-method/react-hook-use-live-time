626f3c17e307fa07c0b20853426b906ed896f4a0
Pontus Alexander <pontus.alexander@gmail.com>
2020-02-18 19:26:25 +0100

    Add useEffect that emits next second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { useState, Component } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import './App.css';
 
 const useLiveDate = () => {
   const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const next = new Date(date.getTime());
+    next.setSeconds(next.getSeconds() + 1);
+    const delayMs = next - date;
+    console.log("Emitting %s in %d", next.toISOString(), delayMs);
+    const timeout = setTimeout(setDate, delayMs, next);
+    return () => clearTimeout(timeout);
+  }, [date, setDate]);
+
   return date;
 }
 
