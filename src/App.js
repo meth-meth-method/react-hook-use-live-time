e00023ab07cd9623df4671761ef1bdc659ff58cb
Pontus Alexander <pontus.alexander@gmail.com>
2020-02-18 19:34:03 +0100

    Convert input to list of values

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ const useLiveDate = () => {
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
-    const next = new Date(date.getTime());
-    next.setSeconds(next.getSeconds() + 1, 0);
+    const values = [
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds() + 1,
+      0,
+    ];
+
+    const next = new Date(...values);
     const delayMs = next - date;
     console.log("Emitting %s in %d", next.toISOString(), delayMs);
     const timeout = setTimeout(setDate, delayMs, next);
